3f7e17b52251f7b060d8070532c0a469044f2455
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-23 20:51:32 -0300

    Deleted ts, added await

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
-var ulPokemons = document.querySelector("#pokemon-list");
-function fetchAndWritePokemons() {
-    for (var i = 1; i < 152; i++) {
-        fetch("https://pokeapi.co/api/v2/pokemon/".concat(i, "/"))
-            .then(function (response) { return response.json(); })["catch"](function (err) {
-            console.log("Error: ".concat(err));
-        })
-            .then(function (data) {
-            console.log(data);
-            ulPokemons === null || ulPokemons === void 0 ? void 0 : ulPokemons.insertAdjacentHTML("beforeend", "\n          <li class=\"card\">\n              <h2 class=\"card-title\">".concat(data.id, ". ").concat(data.name, "</h2>\n              <img class=\"card-image\" src=\"").concat(data.sprites.front_default, "\"/>\n          </li>\n        "));
-        });
-    }
+const ulPokemons = document.querySelector("#pokemon-list");
+
+async function fetchAndWritePokemons() {
+  for (var i = 1; i < 152; i++) {
+    await fetch(`https://pokeapi.co/api/v2/pokemon/${i}/`)
+      .then((response) => response.json())
+      .catch((err) => {
+        console.log(`Error: ${err}`);
+      })
+      .then((data) => {
+        console.log(data);
+        ulPokemons?.insertAdjacentHTML(
+          "beforeend",
+          `
+          <li class="card">
+              <h2 class="card-title">${data.id}. ${data.name}</h2>
+              <img class="card-image" src="${data.sprites.front_default}"/>
+          </li>
+        `
+        );
+      });
+  }
 }
+
 window.onload = function () {
-    fetchAndWritePokemons();
+  fetchAndWritePokemons();
 };
