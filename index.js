bf50cc7427a0ffec185ea48bd2b231a55eca27c2
Santiago Gonzalez <santiagogonzalezbogado@gmail.com>
2022-11-23 11:50:14 -0300

    Move fetching into a function

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 var pokedex = document.querySelector("#pokemon-list");
-console.log("that");
 var key = "myPokemonList";
-// I'm using sessionStorage so that we only ping the PokéAPI only once per tab
-// open, and the storage persist during refreshes which is what we want, after
-// closing the tab it will be deleted.
-if (sessionStorage.getItem(key) !== null) {
-    // If the Pokémon list is present in sessionStorage we do nothing
-    console.log("Found the pokemon list");
-}
-else {
+function fetchAPI(key) {
+    // I'm using sessionStorage so that we only ping the PokéAPI only once per tab
+    // open, and the storage persist during refreshes which is what we want, after
+    // closing the tab it will be deleted.
+    if (sessionStorage.getItem(key) !== null) {
+        // If the Pokémon list is present in sessionStorage we do nothing
+        console.log("Found the pokemon list in the browser storage");
+        return sessionStorage.getItem(key);
+    }
     // If the Pokémon list is not present in sessionStorage we fetch it
-    console.log("Fetching the pokemon list");
-    fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+    console.log("Fetching the pokemon list and saving it locally");
+    return fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
         .then(function (response) { return response.json(); })
         .then(function (json) {
         sessionStorage.setItem(key, JSON.stringify(json));
-        console.log(json);
+        return sessionStorage.getItem(key);
     });
 }
+window.onload = function () {
+    var pokemon_list = fetchAPI(key);
+    console.log(pokemon_list);
+};
--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
-const pokedex = document.querySelector("#pokemon-list");
-console.log("that");
+const pokedex = document.querySelector('#pokemon-list')
 
-var key = "myPokemonList";
+var key = 'myPokemonList'
 
-// I'm using sessionStorage so that we only ping the PokéAPI only once per tab
-// open, and the storage persist during refreshes which is what we want, after
-// closing the tab it will be deleted.
-if (sessionStorage.getItem(key) !== null) {
-  // If the Pokémon list is present in sessionStorage we do nothing
-  console.log(`Found the pokemon list`);
-} else {
+function fetchAPI(key: string) {
+  // I'm using sessionStorage so that we only ping the PokéAPI only once per tab
+  // open, and the storage persist during refreshes which is what we want, after
+  // closing the tab it will be deleted.
+  if (sessionStorage.getItem(key) !== null) {
+    // If the Pokémon list is present in sessionStorage we do nothing
+    console.log(`Found the pokemon list in the browser storage`)
+    return sessionStorage.getItem(key)
+  }
+
   // If the Pokémon list is not present in sessionStorage we fetch it
-  console.log(`Fetching the pokemon list`);
-  fetch("https://pokeapi.co/api/v2/pokemon/?limit=151")
+  console.log(`Fetching the pokemon list and saving it locally`)
+  return fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
     .then((response) => response.json())
     .then((json) => {
-      sessionStorage.setItem(key, JSON.stringify(json));
-      console.log(json);
-    });
+      sessionStorage.setItem(key, JSON.stringify(json))
+      return sessionStorage.getItem(key)
+    })
+}
+
+window.onload = function () {
+  let pokemon_list = fetchAPI(key)
+  console.log(pokemon_list)
 }
